59dfd9670cf5b42d16d6380f38258f9a4f9172b0
Apollo13 <calderon.ig08@estudiantec.cr>
2020-10-21 23:27:06 -0600

    add methods

--- src/model/game/Player.java
+++ src/model/game/Player.java
@@ -1,9 +1,10 @@
 package model.game;
 
-import test.Hola;
+//import test.Hola;
 
 /**
- *
+ * Esta clase se maneja toda la informacion relacionada con el jugador como el nomrbe, vida y mana
+ * @version 1
  */
 public class Player {
 
@@ -24,6 +25,8 @@ public class Player {
      */
     private Conextion typeConexion;
 
+    private int percentaje;
+
     /**
      *
      */
@@ -33,40 +36,55 @@ public class Player {
         this.life = 1000;
     }
 
-
     /**
-     *
+     * Este metodo retorna el nombre del jugador
      */
     public String getName() {
-        return name;
+        return this.name;
     }
 
     /**
-     *
+     * Este metodo retorna el mana del jugador
      */
     public int getMana() {
-        return mana;
+        return this.mana;
     }
 
     /**
-     *
+     * Este metodo se encarga de colocar un nuevo mana
      */
     public void setMana(int mana) {
         this.mana = mana;
-        if(this.mana>200){
-            this.mana = 200;
+        if(this.mana > 1000){
+            this.mana = 1000;
         }
     }
 
     /**
+     * Este metodo se encarga de incrementar en un 25 porciento el mana del jugador
      *
      */
-    public int getLife() {
-        return life;
+    public void increase25() {
+        percentaje = (int) (0.25 * this.mana);
+
+        if (this.mana > 1000){
+            this.mana = 1000;
+        } else if ((this.mana + percentaje) > 1000){
+            this.mana = 1000;
+        } else {
+            this.mana += percentaje;
+        }
     }
 
     /**
-     *
+     * Este metodo retorna la vida del jugador
+     */
+    public int getLife() {
+        return this.life;
+    }
+
+    /**
+     * Este metodo coloca un nuevo entero en la vida del jugador
      */
     public void setLife(int life) {
         this.life = life;
