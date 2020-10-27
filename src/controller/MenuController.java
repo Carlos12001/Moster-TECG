e287a02a0daf25f155f7c950d6489ec42aa23675
Carlos12001 <carlos.andres12001@gmail.com>
2020-10-26 23:37:07 -0600

    Update MenuController.java

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import model.game.ConnectionType;
+import model.sockets.ConnectionType;
 import model.game.*;
 import model.sockets.UpdateInfo;
 
