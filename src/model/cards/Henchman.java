e99eb6f311599d295df2344577a30a8311c51081
Carlos12001 <carlos.andres12001@gmail.com>
2020-11-01 18:32:36 -0600

    All Atacks

--- src/model/cards/Henchman.java
+++ src/model/cards/Henchman.java
@@ -35,16 +35,16 @@ public class Henchman extends Card{
      */
     private void setHenchman(short numCode){
         switch (numCode) {
-            case 0 -> this.setAtack(70);
-            case 1 -> this.setAtack(100);
-            case 2 -> this.setAtack(30);
-            case 3 -> this.setAtack(40);
-            case 4 -> this.setAtack(50);
-            case 5 -> this.setAtack(20);
-            case 6 -> this.setAtack(150);
-            case 7 -> this.setAtack(80);
-            case 8 -> this.setAtack(120);
-            case 9 -> this.setAtack(340);
+            case 0 -> this.setAtack(150);
+            case 1 -> this.setAtack(200);
+            case 2 -> this.setAtack(100);
+            case 3 -> this.setAtack(80);
+            case 4 -> this.setAtack(140);
+            case 5 -> this.setAtack(110);
+            case 6 -> this.setAtack(130);
+            case 7 -> this.setAtack(200);
+            case 8 -> this.setAtack(300);
+            case 9 -> this.setAtack(200);
         }
     }
 
